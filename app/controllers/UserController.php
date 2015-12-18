b68d29384271f3352f19a30b270d9771158e50b3
mikepierre <mikeypierre84@yahoo.com>
2015-12-18 00:01:08 -0500

    Added User Controller
    User Controller is for single user POST, GET, and PUT request for api.

---
+++ app/controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+/*
+* This class gets single user information.
+*/
+class UserController extends Controller
+{
+    /**
+     * POST New information in to user
+     * POST REQUEST
+     * @param  int  $id
+     * @return Response
+     */
+    public function index($id)
+    {
+        //
+    }
+
+    /**
+     * Display single user information
+     * GET REQUEST
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update single user information
+     * PUT REQUEST
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+}
